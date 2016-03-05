d476a557c846d036b8b9af5f780b53582747f962
Han RuiGang <hanruigang@163.com>
2016-03-05 13:42:41 +0800

    code
    this is my yb code

---
+++ yb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yb
+//
+//  Created by sandy on 16/3/5.
+//  Copyright (c) 2016年 sandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
